c2c412486580066298f37e5dadf128d38d9b4775
Andersen Fan <as181920@hotmail.com>
2013-05-04 16:37:20 +0800

    NetHttp instead of EMSynchrony to try resolve problem in rails

--- lib/mn_client/models.rb
+++ lib/mn_client/models.rb
@@ -6,8 +6,8 @@ Her::API.setup url:  MnClient::Configuration.api_url do |connection|
   connection.use Faraday::Request::UrlEncoded
   connection.response :logger
   connection.use Her::Middleware::DefaultParseJSON
-  #connection.use Faraday::Adapter::NetHttp
-  connection.use Faraday::Adapter::EMSynchrony # make http request with eventmachine and synchrony
+  connection.use Faraday::Adapter::NetHttp
+  #connection.use Faraday::Adapter::EMSynchrony # make http request with eventmachine and synchrony
 end
 
 LIB_PATH = File.dirname(File.absolute_path(__FILE__))
